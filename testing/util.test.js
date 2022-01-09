f5c3072d5c8310e4e3f89d40cd31c6eb6c290d8f
Shinhee Park <shinhee.park777@gmail.com>
2022-01-09 12:21:19 -0800

    Labeled unit test

--- testing/util.test.js
+++ testing/util.test.js
@@ -1,6 +1,9 @@
 const { generateText } = require('./util');
 
+
+//unit test
 test('should output name and age', () => {
     const text = generateText('Ellie', 1);
     expect(text).toBe('Ellie (1 years old)');
 });
+
